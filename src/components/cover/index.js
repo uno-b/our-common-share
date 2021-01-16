ea3eb82772c4d06fb4d625208c4dc2133dbb4b5c
Unumandakh <bg.unuu@yahoo.com>
2021-01-16 13:53:32 +0800

    removed earth

--- src/components/cover/index.js
+++ src/components/cover/index.js
@@ -8,9 +8,6 @@ export default () => {
   return (
     <div className={styles.cover}>
       <div className={styles.starrySky}></div>
-      <div className={styles.earth}>
-        <div></div>
-      </div>
       <Particles />
       <Title />
     </div>
