f563007237ec5e6fb92be01698db692e4293f57b
Unumandakh <bg.unuu@yahoo.com>
2021-01-17 01:05:16 +0800

    Added faq section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React from "react"
 import Layout from "../components/layout"
 import Cover from "../components/cover"
 import SectionTwo from "../components/section-two"
+import FAQ from "../components/faq"
 
 const IndexPage = () => {
   return (
     <Layout>
       <Cover />
       <SectionTwo />
+      <FAQ />
     </Layout>
   )
 }
