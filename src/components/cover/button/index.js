4d442be158f734b50e867afde9a3c4468729dc95
Unumandakh <bg.unuu@yahoo.com>
2021-01-23 00:37:31 +0800

    Added scroll animation to cover button

--- src/components/cover/button/index.js
+++ src/components/cover/button/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import { FaArrowDown } from "react-icons/fa"
+import scrollTo from "gatsby-plugin-smoothscroll"
+
 import styles from "./button.module.css"
 
 const Button = () => {
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={() => scrollTo("#about")}>
       <FaArrowDown />
     </button>
   )
