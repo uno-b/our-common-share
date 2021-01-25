b47342c4dbe8c4bc4d5d9037ded40c39bf38a47a
Unumandakh <bg.unuu@yahoo.com>
2021-01-26 00:03:20 +0800

    Fixed fundraising and contact sections

--- src/components/fundraising/index.js
+++ src/components/fundraising/index.js
@@ -6,36 +6,32 @@ const Fundraising = () => {
   return (
     <>
       <div id="fundraising" />
-      <div className={styles.container}>
-        <div data-sal="slide-up">
-          <h1 className={styles.sectionTitle}>Fundraising</h1>
-          <div className={styles.underline}></div>
+      <div styles={styles.container}>
+        <div className={styles.wrapper}>
+          <div data-sal="slide-up">
+            <h1 className={styles.sectionTitle}>Fundraising</h1>
+            <div className={styles.underline}></div>
+          </div>
+          <ul
+            className={styles.list}
+            data-sal="slide-up"
+            data-sal-delay="200"
+            data-sal-duration="1000"
+          >
+            <motion.li whileHover={{ scale: 1.1 }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+              accusamus quidem saepe itaque asperiores quasi nulla incidunt
+              ullam delectus libero ut nisi, autem eum iure nihil deserunt qui.
+              Veritatis, consequatur.
+            </motion.li>
+            <motion.li whileHover={{ scale: 1.1 }}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A
+              accusamus quidem saepe itaque asperiores quasi nulla incidunt
+              ullam delectus libero ut nisi, autem eum iure nihil deserunt qui.
+              Veritatis, consequatur.
+            </motion.li>
+          </ul>
         </div>
-        <ul
-          className={styles.list}
-          data-sal="slide-up"
-          data-sal-delay="200"
-          data-sal-duration="1000"
-        >
-          <motion.li whileHover={{ scale: 1.1 }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A accusamus
-            quidem saepe itaque asperiores quasi nulla incidunt ullam delectus
-            libero ut nisi, autem eum iure nihil deserunt qui. Veritatis,
-            consequatur.
-          </motion.li>
-          <motion.li whileHover={{ scale: 1.1 }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A accusamus
-            quidem saepe itaque asperiores quasi nulla incidunt ullam delectus
-            libero ut nisi, autem eum iure nihil deserunt qui. Veritatis,
-            consequatur.
-          </motion.li>
-          <motion.li whileHover={{ scale: 1.1, duration: 1000 }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. A accusamus
-            quidem saepe itaque asperiores quasi nulla incidunt ullam delectus
-            libero ut nisi, autem eum iure nihil deserunt qui. Veritatis,
-            consequatur.
-          </motion.li>
-        </ul>
       </div>
     </>
   )
