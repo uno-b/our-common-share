b2772784ff1da343618e165be1ac279feb68db29
Unumandakh <bg.unuu@yahoo.com>
2021-01-26 16:29:23 +0800

    Added animation to cover button

--- src/components/cover/button/index.js
+++ src/components/cover/button/index.js
@@ -1,14 +1,19 @@
 import React from "react"
 import { FaArrowDown } from "react-icons/fa"
 import scrollTo from "gatsby-plugin-smoothscroll"
+import { motion } from "framer-motion"
 
 import styles from "./button.module.css"
 
 const Button = () => {
   return (
-    <button className={styles.button} onClick={() => scrollTo("#about")}>
+    <motion.button
+      animate={{ scale: [1.0, 1.2], transition: { yoyo: Infinity } }}
+      className={styles.button}
+      onClick={() => scrollTo("#about")}
+    >
       <FaArrowDown />
-    </button>
+    </motion.button>
   )
 }
 
