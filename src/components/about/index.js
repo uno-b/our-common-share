4ddd1aa81f84adbbf2780a798f98c29e0d3836d3
Unumandakh <bg.unuu@yahoo.com>
2021-01-30 19:19:41 +0800

    Changed info in about section

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,7 +1,9 @@
 import React from "react"
-import img1 from "../../images/social-media.webp"
-import img2 from "../../images/nodes.webp"
+
+import img1 from "../../assets/logo2.png"
+import img2 from "../../images/social-media.webp"
 import img3 from "../../images/get-paid.webp"
+import img4 from "../../images/nodes.webp"
 import styles from "./about.module.css"
 
 const About = () => {
@@ -20,7 +22,8 @@ const About = () => {
         >
           <img
             src={img1}
-            alt="Social Media"
+            className={styles.logo}
+            alt="Logo"
             data-sal="slide-right"
             data-sal-duration="700"
           />
@@ -30,17 +33,17 @@ const About = () => {
               data-sal-duration="700"
               data-sal-delay="200"
             >
-              Why a new social space?
+              Introduction
             </h2>
             <p
               data-sal="slide-left"
               data-sal-duration="700"
               data-sal-delay="400"
             >
-              Some top founders of the first generational social media have
-              admitted to their faults. People are giving up their autonomy.
-              Both social media (Twitter, Facebook) do things to maximize
-              "stickiness".
+              Our Common Share™ OCS™, a revolutionary new system of social
+              media, along with community managed governance, and a currency
+              token for its market systems, built on Custom Blockchain
+              technology.
             </p>
             <button>Continue</button>
           </div>
@@ -62,18 +65,24 @@ const About = () => {
               data-sal-duration="700"
               data-sal-delay="200"
             >
-              Tokenized Trust
+              Why Create a New Social Space?
             </h2>
             <p
               data-sal="slide-left"
               data-sal-duration="700"
               data-sal-delay="400"
             >
-              The world’s first token-trust empowered Social Exchange™ -and an
-              entirely new vision of the social Internet. Grow user base through
-              extending a "Common Share" of token ownership. Users will have
-              their own beautiful space with AI support. + copyright protection
-              for user content.
+              In recent months the world’s leading tech executives including Tim
+              Cook and even Mark Zukerberg have publicly acknowledged the
+              dysfunctional nature of 1st generation social platforms. Sean
+              Parker, Facebook co-founder, now calls himself a social media
+              “conscientious objector.” Former Facebook exec Chamath
+              Palihapitiya went so far as to apologize for his part in creating
+              “tools that are ripping apart the social fabric of how society
+              works”. At Davos Georges Soros went beyond the “addiction”
+              critique of psychologists, public health and youth workers and
+              political science researchers to say that the giants of social
+              “are inducing people to give up their autonomy…
             </p>
             <button>Continue</button>
           </div>
@@ -95,18 +104,55 @@ const About = () => {
               data-sal-duration="700"
               data-sal-delay="200"
             >
-              Respect and Empowerment
+              Get Paid To Use OCS™ Social Exchange™
             </h2>
             <p
               data-sal="slide-left"
               data-sal-duration="700"
               data-sal-delay="400"
             >
-              It will offer a better-designed and uncluttered space for users
-              plus tools to help realize self-expression, communication and
-              collaboration. Privacy invasion, data mining and ad push won’t be
-              features + granular customizability, full user control over
-              ad-push and data sharing plus user designated communities.
+              OCS™ offers a system that at its core is about sharing with its
+              community. Our Common Share™ Social Exchange™ is about freedom of
+              expression, artistic expression, and free speech, with real
+              people. Content drives social media, OCS™ recognizes the value of
+              user driven content, and we want to reward you for it. So we want
+              to set aside a % of OCS™ profits into the OCS™ member share, a
+              community owned, and governed fund.
+            </p>
+            <button>Continue</button>
+          </div>
+        </div>
+        <div
+          className={styles.item}
+          data-sal="slide-up"
+          data-sal-duration="700"
+        >
+          <img
+            src={img4}
+            alt="Social Media"
+            data-sal="slide-right"
+            data-sal-duration="700"
+          />
+          <div className={styles.info}>
+            <h2
+              data-sal="slide-left"
+              data-sal-duration="700"
+              data-sal-delay="200"
+            >
+              Have you noticed how toxic some social media has become?
+            </h2>
+            <p
+              data-sal="slide-left"
+              data-sal-duration="700"
+              data-sal-delay="400"
+            >
+              Our Common Share™ Ethos will be derived from a mix of internet
+              charters, as well as the Canadian Charter of Rights and Freedoms,
+              the American Bill of Rights, and the Universal Declaration of
+              Human Rights (UDHR). This will put the power of community control
+              back into the systems we use daily. Whether it's creating, coding,
+              writing, sharing, or remembering, OCS™ users will have a say in
+              the creation and management of these ethos.
             </p>
             <button>Continue</button>
           </div>
---
+++ src/components/info/index.js
@@ -0,0 +1,101 @@
+import React from "react"
+import styles from "./info.module.css"
+
+const Info = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.intro}>
+        <div className={styles.textBlock}>
+          <p>
+            Our Common Share™ OCS™, a revolutionary new system of social media,
+            along with community managed governance, and a currency token for
+            its market systems, built on Custom Blockchain technology.
+          </p>
+        </div>
+        <div className={styles.textBlock}>
+          <p>
+            Traditional social networking is a system of information shared by
+            users that is used by the centralized platforms to drive traffic to
+            links that can increase profits, along with background data
+            collection. This raises issues regarding privacy, ownership of
+            information, and protection of data, that privately owned,
+            centralized systems, are not managing in a manner that respects
+            users rights to privacy.
+          </p>
+          <p>
+            By using a combination of machine learning and human verification
+            advertising, clickbait, malware, and the minefield of other junk can
+            be mitigated. Our Community Advertising Committee will be a human
+            verification system, that examines ads by thoroughly verifying their
+            content, legitimacy,and other metrics. In other words, it shields
+            OCS™ from invasive advertising, fake news and much more. There is no
+            system that beats a human verification process. Ads will have to
+            comply with community standards.
+          </p>
+          <p>
+            Data traditionally mined from the platform user will be shown to the
+            user and the user can decide what they want to share, if anything.
+            There will be the option to monetize this data, but it will not be
+            on by default. This gives members a chance to learn about privacy,
+            and data management before activation.
+          </p>
+        </div>
+        <div className={styles.textBlock}>
+          <p>
+            Traditional social networking is a system of information shared by
+            users that is used by the centralized platforms to drive traffic to
+            links that can increase profits, along with background data
+            collection. This raises issues regarding privacy, ownership of
+            information, and protection of data, that privately owned,
+            centralized systems, are not managing in a manner that respects
+            users rights to privacy.
+          </p>
+        </div>
+        <div className={styles.textBlock}>
+          <p>
+            Together with the people of this world we can, and will build
+            transparent social media, a real community, of actual people, and
+            take back control to the user. No more big business directing the
+            narrative, or politics. The people will be the voice of change, you
+            have the power to simply sign up and use this powerful new platform
+            to get more involved in the direction of OCS™, that puts the real
+            power of change into the hands of the people! We realize this new
+            direction is needed in our social media systems.
+          </p>
+          <p>
+            Companies joining the platform, user created or existing will apply
+            to community for licence to create their space in OCS™, a panel of
+            community members will score the company on a few different metrics,
+            such as, community involvement, employee satisfaction, environmental
+            stewardship, and other metrics the community decides are relevant in
+            the process. A vote will be held and the company will be granted
+            space on the platform, their score average displayed, this score
+            will be a link to the breakdown of score across categories, and a
+            forum the company can use to work with the OCS™ community to get
+            that score to 100% OCS™ will create a environment where people will
+            truly have a choice, a vote, a share, and most importantly, a voice!
+          </p>
+        </div>
+      </div>
+      <div className={styles.socialExchange}></div>
+      <div className={styles.toxic}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
+          temporibus quas distinctio, alias inventore animi reiciendis similique
+          officiis architecto, illum libero, tempora pariatur quidem
+          exercitationem unde quaerat. Quos, eum veritatis.
+        </p>
+      </div>
+      <div className={styles.socialSpace}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
+          temporibus quas distinctio, alias inventore animi reiciendis similique
+          officiis architecto, illum libero, tempora pariatur quidem
+          exercitationem unde quaerat. Quos, eum veritatis.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Info
